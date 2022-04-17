37117efb9d2139ce2874f4c971b0ac6c9949aa15
Sean Orosz <seanorosz@yahoo.com>
2022-04-17 11:19:31 -0400

    added style and formatting

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const populate = (obj) => {
+    const wrapper = document.querySelector('#wrapper');
     console.log(obj);
-}
-
-const init = () => {
-    fetchJson().then((obj) => {
-        populate(obj);
+    obj.forEach(cat => {
+        let catHolder = document.createElement('div');
+        catHolder.className += 'cat-div';
+        let catName = document.createElement('h2');
+        let catInfo = document.createElement('p');
+        let kittenNames = [];
+        cat.kittens.forEach(kitten => {
+            kittenNames.push(kitten.name);
+        });
+        catInfo.textContent = `${cat.name} is a ${cat.color} ${cat.breed} with ${cat.kittens.length} kittens. Their names are ${kittenNames}`;
+        catName.textContent = cat.name;
+        catHolder.appendChild(catName);
+        catHolder.appendChild(catInfo);
+        wrapper.appendChild(catHolder);
     });
 }
 
-window.onload = () => {
-    init();
-}
+const init = () => fetchJson().then(obj => populate(obj));
+
+window.onload = () => init();
